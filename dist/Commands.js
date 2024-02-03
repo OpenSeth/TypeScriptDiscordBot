8857be109b91b265db043677d360b87ac9543a47
FatCake <sethseavers@gmail.com>
2024-02-02 22:33:46 -0600

    Deleting dist

--- dist/Commands.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Commands = void 0;
-const Hello_1 = require("./commands/Hello");
-const ThisIsBeans_1 = require("./commands/ThisIsBeans");
-exports.Commands = [Hello_1.Hello, ThisIsBeans_1.ThisIsBeans];
